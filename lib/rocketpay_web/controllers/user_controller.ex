561d443dde001eff173619a531b08f951c9c1ac0
Pliavi <pliavi.rpgm@gmail.com>
2021-03-12 13:42:53 -0300

    feat: create update and delete users

--- lib/rocketpay_web/controllers/user_controller.ex
+++ lib/rocketpay_web/controllers/user_controller.ex
@@ -2,6 +2,7 @@ defmodule RocketpayWeb.UserController do
   use RocketpayWeb, :controller
 
   alias Rocketpay.Users
+  alias Rocketpay.Repo
 
   action_fallback Rocketpay.FallbackController
 
@@ -10,6 +11,22 @@ defmodule RocketpayWeb.UserController do
       conn
       |> put_status(:created)
       |> render("create.json", user: user)
+    end
+  end
+
+  def update(conn, %{"id" => id, "params" => params}) do
+    user = Repo.get!(Account, id)
+
+    with {:ok, updated_user} <- Users.update(user, params) do
+      render(conn, "update.json", user: updated_user)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    user = Repo.get!(Account, id)
+
+    with {:ok, deleted_user} <- Users.delete(user) do
+      render(conn, "delete.json", user: deleted_user)
     end
   end
 end

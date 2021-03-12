24625a41c799c71c98528b0b30767a9edbf4b0ec
Pliavi <pliavi.rpgm@gmail.com>
2021-03-12 13:42:13 -0300

    refactor: make block use only one functin

--- lib/rocketpay/account/accounts.ex
+++ lib/rocketpay/account/accounts.ex
@@ -7,13 +7,10 @@ defmodule Rocketpay.Accounts do
     |> Repo.insert()
   end
 
-  def block(%{id: id}), do: set_block_date(id, :block)
-  def unblock(%{id: id}), do: set_block_date(id, :unblock)
+  def set_block_state(id, :block), do: set_block_date(id, NaiveDateTime.utc_now())
+  def set_block_state(id, :unblock), do: set_block_date(id, nil)
 
-  defp set_block_date(id, :block), do: do_set_block_date(id, NaiveDateTime.utc_now())
-  defp set_block_date(id, :unblock), do: do_set_block_date(id, nil)
-
-  defp do_set_block_date(id, blocked_at) do
+  defp set_block_date(id, blocked_at) do
     Repo.get(Account, id)
     |> Account.changeset(%{blocked_at: blocked_at})
     |> Repo.update()

a9210a3150ca8cd73fcf2f1c9d1f82d080de2a34
Pliavi <pliavi.rpgm@gmail.com>
2021-03-12 11:39:17 -0300

    remove blocked_at from rrquired atributes

--- lib/rocketpay/account/account.ex
+++ lib/rocketpay/account/account.ex
@@ -8,7 +8,7 @@ defmodule Rocketpay.Account do
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
 
-  @required_attrs [:balance, :blocked_at, :user_id]
+  @required_attrs [:balance, :user_id]
 
   schema "accounts" do
     field :balance, :decimal

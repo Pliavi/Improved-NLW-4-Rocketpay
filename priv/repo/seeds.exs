8432958cb10090793208e373259f19b57c587e97
Pliavi <pliavi.rpgm@gmail.com>
2021-03-12 11:52:24 -0300

    test: remove seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,20 +1,20 @@
 # Script for populating the database. You can run it as:
 #
 #     mix run priv/repo/seeds.exs
-[
-  %{
-    "name" => "fulano",
-    "username" => "fulano",
-    "email" => "fulano@fulano.com",
-    "age" => "25",
-    "password" => "123456"
-  },
-  %{
-    "name" => "ciclano",
-    "username" => "ciclano",
-    "email" => "ciclano@ciclano.com",
-    "age" => "20",
-    "password" => "123456"
-  }
-]
-|> Enum.map(&Rocketpay.create_user/1)
+# [
+#   %{
+#     "name" => "fulano",
+#     "username" => "fulano",
+#     "email" => "fulano@fulano.com",
+#     "age" => "25",
+#     "password" => "123456"
+#   },
+#   %{
+#     "name" => "ciclano",
+#     "username" => "ciclano",
+#     "email" => "ciclano@ciclano.com",
+#     "age" => "20",
+#     "password" => "123456"
+#   }
+# ]
+# |> Enum.map(&Rocketpay.create_user/1)

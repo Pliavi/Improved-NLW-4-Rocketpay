8e258ffe16d0b3417aad460793ae3e39fa598905
Pliavi <pliavi.rpgm@gmail.com>
2021-03-31 19:43:29 -0300

    fix: type can't be used as column and fix timestamp

--- priv/repo/migrations/20210312200224_create_bank_statement_table.exs
+++ priv/repo/migrations/20210312200224_create_bank_statement_table.exs
@@ -4,11 +4,14 @@ defmodule Rocketpay.Repo.Migrations.CreateBankStatementTable do
   def change do
     create table :bank_statement do
       add :value, :decimal
-      add :user_id, references(:users, type: :binary_id)
-      add :type, :string # TODO: Create a type table
+      add :account_id, references(:accounts, type: :binary_id)
+      # TODO: Create a operation table
+      add :operation, :string
+      # TODO: Create a suboperation table
+      add :sub_operation, :string
       add :reason, :string
 
-      timestamps(:updated_at: false, type: :utc_datetime_usec)
+      timestamps(updated_at: false, type: :utc_datetime_usec)
     end
   end
 end
